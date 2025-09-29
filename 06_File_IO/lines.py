9aa074532aafc3f2c84cc06915be1c15ff2fae62
Teodor <teodor.jendrisak.com>
2025-09-29 16:37:39 +0200

    Add lines.py code and temp testing file

--- 06_File_IO/lines.py
+++ 06_File_IO/lines.py
@@ -6,21 +6,26 @@ def main():
         sys.exit("Missing command-line argument")
     elif len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
-    elif sys.argv[1].endswith(".py") == False:
+    if not sys.argv[1].endswith(".py"):
         sys.exit("Not a python file")
 
     try:
-        lines = []
-
-        input = sys.argv[1]
-        with open(input) as file:
+        filename = sys.argv[1]
+        line_count = 0
+        with open(filename) as file:
             for line in file:
-                lines.append(line)
+                stripped = line.strip()
+                if not stripped:
+                    continue
+                if stripped.startswith("#"):
+                    continue
+                else:
+                    line_count += 1
     except FileNotFoundError:
         sys.exit("File does not exist")
 
-    for line in lines:
-        #you left off here - checking if lines is empty
+    print(line_count)
+
 
 if __name__ == "__main__":
     main()
--- 06_File_IO/temp.py
+++ 06_File_IO/temp.py
@@ -1,3 +1,6 @@
 Hello there
 My name is Teo
 I am 26 years old
+       
+#comment 1
+#comment 2
